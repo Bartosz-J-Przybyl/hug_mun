f58c606f436b00a1444b37499c279c12cc07b612
ShiroganeS <104523638+ShiroganeS@users.noreply.github.com>
2024-04-05 18:33:13 +0200

    improve auth screen and add  validations to user input

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:hug_mun/widgets/reusable_text_form_field.dart';
 
@@ -9,47 +11,136 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  var isLogin = true;
+  var _enteredEmail = "";
+  var _enteredPassword = "";
+  final _formKey = GlobalKey<FormState>();
+  void _submit() {
+    final isValid = _formKey.currentState!.validate();
+    if (isValid) {
+      _formKey.currentState!.save();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.onPrimary,
+      backgroundColor: Colors.black,
       body: Center(
-        child: Stack(
-          alignment: AlignmentDirectional.center,
-          clipBehavior: Clip.none,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Positioned(
-              top: -155,
-              child: Image.asset(
-                "lib/assets/images/kruk.png",
-                width: 300,
-              ),
-            ),
-            Card(
-              color:
-                  Theme.of(context).colorScheme.onBackground.withOpacity(0.30),
-              margin: const EdgeInsets.all(20),
-              child: const SingleChildScrollView(
-                child: Padding(
-                  padding:
-                      EdgeInsets.only(left: 16, right: 16, top: 20, bottom: 25),
-                  child: Form(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        ReusableTextFormField(
-                          labelText: "Email Address",
-                          keyboardType: TextInputType.emailAddress,
-                          obscureText: false,
+            Stack(
+              alignment: AlignmentDirectional.center,
+              clipBehavior: Clip.none,
+              children: [
+                Align(
+                  child: Container(
+                    height: MediaQuery.of(context).size.width,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Theme.of(context).colorScheme.background),
+                  ),
+                ),
+                BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 80.0, sigmaY: 80.0),
+                  child: Container(),
+                ),
+                Positioned(
+                  top: -10,
+                  child: Image.asset(
+                    "lib/assets/images/kruk.png",
+                    width: 300,
+                  ),
+                ),
+                Positioned(
+                  top: 145,
+                  width: 400,
+                  height: 240,
+                  child: Card(
+                    color: Theme.of(context)
+                        .colorScheme
+                        .onBackground
+                        .withOpacity(0.30),
+                    margin: const EdgeInsets.all(20),
+                    child: SingleChildScrollView(
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            left: 16, right: 16, top: 20, bottom: 25),
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              ReusableTextFormField(
+                                labelText: "Email Address",
+                                keyboardType: TextInputType.emailAddress,
+                                obscureText: false,
+                                validator: (value) {
+                                  if (value == null ||
+                                      value.trim().isEmpty ||
+                                      !value.contains("@")) {
+                                    return "Please enter a valid email address";
+                                  }
+                                  return null;
+                                },
+                                onSave: (value) {
+                                  _enteredEmail = value!;
+                                },
+                              ),
+                              ReusableTextFormField(
+                                  onSave: (value) {
+                                    _enteredPassword = value!;
+                                  },
+                                  validator: (value) {
+                                    if (value == null ||
+                                        value.trim().isEmpty ||
+                                        value.trim().length < 6) {
+                                      return "Password must be at least 6 characters long";
+                                    }
+                                    return null;
+                                  },
+                                  labelText: "Password",
+                                  obscureText: true),
+                            ],
+                          ),
                         ),
-                        ReusableTextFormField(
-                            labelText: "Password", obscureText: true),
-                      ],
+                      ),
                     ),
                   ),
                 ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.background),
+              onPressed: _submit,
+              child: Text(
+                isLogin ? "Login" : "Sign Up",
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.onBackground),
               ),
-            )
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  isLogin = !isLogin;
+                });
+              },
+              child: Text(
+                isLogin ? "Create a account" : "Login",
+                style:
+                    TextStyle(color: Theme.of(context).colorScheme.background),
+              ),
+            ),
           ],
         ),
       ),
